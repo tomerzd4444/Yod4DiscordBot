75fd6fcfed7a5a60c9a177d74b91b61c970019ae
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2020-10-17 00:33:31 +0300

    added activity
    bot now watches zoom lessons

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix="please ")
+bot = commands.Bot(command_prefix="please ", self_bot=True)
 load_dotenv(dotenv_path="token.env")
 token = os.getenv('token')
 homeworkDict = {}
 print(token)
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the zoom lesson"))
 
 def printDict(d: dict):
     toReturn = ""
@@ -17,17 +21,10 @@ def printDict(d: dict):
         toReturn += f"{key}: {', '.join(value)} \n"
     return toReturn
 
-
 def addWork(lessonName: str, work: str):
     if not homeworkDict.get(lessonName):
         homeworkDict[lessonName] = []
     homeworkDict[lessonName].append(work)
-
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-
 
 @bot.command()
 async def homework(ctx: discord.ext.commands.context.Context):
