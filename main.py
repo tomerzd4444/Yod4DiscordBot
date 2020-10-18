b9433cc1b046df324d90aea04dc9438b7f396ced
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2020-10-18 09:34:13 +0300

    updated prefix and command names

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.utils import get
 from dotenv import load_dotenv
 from datetime import datetime
 
-bot = commands.Bot(command_prefix="please ")
+bot = commands.Bot(command_prefix="hw ")
 load_dotenv(dotenv_path="token.env")
 token = os.getenv('token')
 homeworkDict = {}
@@ -24,24 +24,13 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the zoom lesson"))
 
-def printDict(d: dict):
-    toReturn = ""
-    for key, value in d.items():
-        toReturn += f"{key}: {', '.join(value)} \n"
-    return toReturn
-
-def addWork(subject: str, work: str):
-    if not homeworkDict.get(subject):
-        homeworkDict[subject] = []
-    homeworkDict[subject].append(work)
-
 @bot.command()
 async def homework(ctx: discord.ext.commands.context.Context):
     print(type(ctx))
     await ctx.send("working")
 
-@bot.command()
-async def setHomework(ctx: discord.ext.commands.context.Context, date:str, subject: str, *work: str):
+@bot.command
+async def sethw(ctx: discord.ext.commands.context.Context, date:str, subject: str, *work: str):
     parsedTime = datetime.strptime(date, "%d-%m-%Y")
     workString = ' '.join(work)
     if subject.lower() not in map(str.lower, subjects): #Checking if the subject exists.
@@ -56,7 +45,7 @@ async def setHomework(ctx: discord.ext.commands.context.Context, date:str, subje
 
 
 @bot.command()
-async def getHomework(ctx: discord.ext.commands.context.Context):
+async def gethw(ctx: discord.ext.commands.context.Context):
     for s in subjects:
         embed = discord.Embed(color=0x1f49ab, title=s)
         mycursor = mydb.cursor()
