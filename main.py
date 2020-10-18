9c141c446f75d2d0d6acc62a25457e083dee6f06
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2020-10-18 14:38:03 +0300

    Added manual delete and automatic delete

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
 from datetime import datetime
+import time
+import threading
 
-bot = commands.Bot(command_prefix="hw ")
+bot = commands.Bot(command_prefix=";")
 load_dotenv(dotenv_path="token.env")
 token = os.getenv('token')
-homeworkDict = {}
-subjects = ["Math", "English", "Hebrew", "Biology", "Cinema", "Arabic", "Tanah", "History", "Literature", "SocialStudies", "Yigal", "Sport", "CS", "Physics", "Chemistry"]
+subjectList = ["Math", "English", "Hebrew", "Biology", "Cinema", "Arabic", "Tanah", "History", "Literature", "SocialStudies", "Yigal", "Sport", "CS", "Physics", "Chemistry"]
 print(token)
 mydb = mysql.connector.connect(
     host="192.168.1.137",
@@ -19,14 +20,22 @@ mydb = mysql.connector.connect(
     database="yod4"
 )
 
+def autoDelete():
+    while True:
+        mycursor = mydb.cursor()
+        mycursor.execute("DELETE FROM homework WHERE date < DATE(\'" + datetime.now().strftime("%Y-%m-%d") + "\')")
+        mydb.commit()
+        time.sleep(3600)
+delThread = threading.Thread(target=autoDelete())
+delThread.start()
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the zoom lesson"))
 
 @bot.command()
-async def getSubjects(ctx: discord.ext.commands.contect.Context):
-    await ctx.send(f'subjects: {subjects'})
+async def subjects(ctx: discord.ext.commands.context.Context):
+    await ctx.send(f'subjects: {subjectList}')
     
 @bot.command()
 async def homework(ctx: discord.ext.commands.context.Context):
@@ -34,28 +43,27 @@ async def homework(ctx: discord.ext.commands.context.Context):
     await ctx.send("working")
 
 @bot.command
-async def sethw(ctx: discord.ext.commands.context.Context, date:str, subject: str, *work: str):
+async def sethw(ctx: discord.ext.commands.context.Context, date: str, subject: str, *work: str):
     parsedTime = datetime.strptime(date, "%d-%m-%Y")
     workString = ' '.join(work)
-    if subject.lower() not in map(str.lower, subjects): #Checking if the subject exists.
+    if subject.lower() not in map(str.lower, subjectList): #Checking if the subject exists.
         await ctx.send(f"There is no such subject.")
         return
     mycursor = mydb.cursor()
     query = "INSERT INTO homework (work, subject, date) VALUES (%s, %s, %s)"
-    val = (workString, subject, parsedTime)
+    val = (workString, subjectList, parsedTime)
     mycursor.execute(query, val) #Adding the new assignment, it automatically gets indexed by subject and date and by date alone
     mydb.commit()
     await ctx.send(f"Set the homework of the lesson {subject.lower()}!")
 
-
 @bot.command()
 async def gethw(ctx: discord.ext.commands.context.Context):
-    for s in subjects:
+    for s in subjectList:
         embed = discord.Embed(color=0x1f49ab, title=s)
         mycursor = mydb.cursor()
         mycursor.execute("SELECT work, date FROM homework WHERE subject = \'"+s+"\' ORDER BY date ASC")
         myresult = mycursor.fetchall()
-        if(mycursor.rowcount > 0):
+        if mycursor.rowcount > 0:
             embedWork = "" #The date and work for each result, we add them together so the embed string looks good and side by side
             embedDate = ""
             for x in myresult: #Adding embed dates and embed works for all results
@@ -64,5 +72,16 @@ async def gethw(ctx: discord.ext.commands.context.Context):
             embed.add_field(name="Work:", value=embedWork, inline=True)
             embed.add_field(name="Date:", value=embedDate, inline=True)
             await ctx.send(embed=embed)
+
+@bot.command()
+async def delhw(ctx: discord.ext.commands.context.Context, *work: str):
+    workString = ' '.join(work)
+    if workString == '':
+        await ctx.send("Please provide an assignment to delete.")
+        return
+    mycursor = mydb.cursor()
+    mycursor.execute("DELETE FROM homework WHERE work = \'" + workString +"\'")
+    mydb.commit()
+    await ctx.send("Deleted assignment.")
 
 bot.run(token)
